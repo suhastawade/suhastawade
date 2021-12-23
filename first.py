d962061502f53540eb89c4bfcae2dfa0c39c4fde
suhastawade <t.suhas1@gmail.com>
2021-12-23 19:37:41 +0530

    First revision of this file. python 2.x to 3.x

--- first.py
+++ first.py
@@ -1,11 +1,13 @@
 #!/usr/sbin/python
 
-def callfunc():
+def callfunc(num1, num2):
     print(f"Calling this function now...")
+    return num1 * num2
 
 
 def main():
-    callfunc()
+    result = callfunc(4, 5)
+    print(f"Final output is {result}")
 
 if __name__ == "__main__":
     main()
